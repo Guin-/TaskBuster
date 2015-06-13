a9f55297507a9944a3a0a1104257cf4d594f1b12
Angelika Jarosz <angelikajarosz@gmail.com>
2015-06-13 16:27:26 -0400

    settings for postgres

--- taskbuster/settings/testing.py
+++ taskbuster/settings/testing.py
@@ -3,3 +3,13 @@ from .base import *
 
 DEBUG = True
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'taskbuster_db',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': '',
+        'PORT': '',
+    }
+}
